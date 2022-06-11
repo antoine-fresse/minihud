dba850ba4faf06a1bfe5f00fdb6335f41fedc677
Jordan Mattingley <jmattingley23@gmail.com>
2022-06-11 01:33:10 -0700

    updated mixin

--- src/main/java/fi/dy/masa/minihud/mixin/MixinScreen.java
+++ src/main/java/fi/dy/masa/minihud/mixin/MixinScreen.java
@@ -2,7 +2,6 @@ package fi.dy.masa.minihud.mixin;
 
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,14 +14,13 @@ public abstract class MixinScreen extends net.minecraft.client.gui.screen.Screen
         super(Text.empty());
     }
 
-    protected net.minecraft.client.MinecraftClient client;
-
     @Inject(method = "sendMessage(Ljava/lang/String;Z)V", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/client/network/ClientPlayerEntity;sendChatMessage(Ljava/lang/String;Lnet/minecraft/text/Text;)V"),
             cancellable = true)
     private void onSendMessage(String msg, boolean addToChat, CallbackInfo ci)
     {
+        assert this.client != null;
         if (DataStorage.getInstance().onSendChatMessage(this.client.player, msg))
         {
             ci.cancel();
